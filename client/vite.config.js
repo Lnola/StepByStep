b0c6484247b544557563df4c424d6a612c6b0d9a
Lnola <nola.luka@gmail.com>
2022-10-26 23:46:50 +0200

    Add absolute path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     port: 3000,
   },
