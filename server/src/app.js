06b0b6dceeff14824521318b68876d857024084b
Lnola <nola.luka@gmail.com>
2022-11-07 23:36:16 +0100

    Implement db connection test

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import router from '@/router';
+import sequelize from '@/shared/database/index';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use('/api', router);
+
+// test database connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to the database.');
+  } catch {
+    console.error('Unable to connect to the database');
+  }
+})();
 
 app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
