22b7360bae1c31b2b4d0d63a7f8e0776d6fe2cbc
Lnola <nola.luka@gmail.com>
2022-11-09 14:39:15 +0100

    Introduce register controller method

--- server/src/user/user.controller.js
+++ server/src/user/user.controller.js
@@ -1,7 +1,8 @@
-import { OK, UNAUTHORIZED } from 'http-status';
+import { CONFLICT, CREATED, OK, UNAUTHORIZED } from 'http-status';
 import errorMessages from '@/shared/constants/errorMessages';
 import HttpError from '@/shared/error/httpError';
 import { setAuthCookies } from '@/shared/helpers/auth';
+import { UniqueConstraintError } from 'sequelize';
 import { User } from '@/shared/database/index';
 
 const login = async (req, res, next) => {
@@ -18,4 +19,21 @@ const login = async (req, res, next) => {
   return res.status(OK).json({ ...user.profile });
 };
 
-export { login };
+const register = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const user = await User.create(body);
+
+    const tokens = await user.generateTokens();
+    setAuthCookies(tokens, res);
+
+    return res.status(CREATED).json({ ...user.profile });
+  } catch (err) {
+    if (err instanceof UniqueConstraintError) {
+      return next(new HttpError(CONFLICT, errorMessages.REGISTER_ERROR));
+    }
+    return next(new Error());
+  }
+};
+
+export { login, register };
