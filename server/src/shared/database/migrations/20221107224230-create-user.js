69516a431b7810343c85e672b898698c49a256cb
Lnola <nola.luka@gmail.com>
2022-11-09 14:51:49 +0100

    Update users migration with new params

--- server/src/shared/database/migrations/20221107224230-create-user.js
+++ server/src/shared/database/migrations/20221107224230-create-user.js
@@ -1,21 +1,31 @@
+const TABLE_NAME = 'users';
+
 module.exports = {
   up(queryInterface, Sequelize) {
-    return queryInterface.createTable('users', {
+    return queryInterface.createTable(TABLE_NAME, {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
       password: {
         allowNull: false,
+        type: Sequelize.STRING,
+      },
+      refreshToken: {
+        allowNull: true,
         type: Sequelize.STRING,
       },
     });
   },
 
   down(queryInterface) {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable(TABLE_NAME);
   },
 };
