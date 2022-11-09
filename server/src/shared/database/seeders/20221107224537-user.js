9906b37dbbb33ef4e99c9f981f5dd61acfbc30e9
Lnola <nola.luka@gmail.com>
2022-11-09 15:04:16 +0100

    Add user seed with password hashing and username

--- server/src/shared/database/seeders/20221107224537-user.js
+++ server/src/shared/database/seeders/20221107224537-user.js
@@ -1,9 +1,23 @@
+const bcrypt = require('bcrypt');
+
+const TABLE_NAME = 'users';
+
+const createUser = index => {
+  const password = bcrypt.hashSync('123', process.env.SALT_ROUNDS || 12);
+  return {
+    username: `example${index}`,
+    password,
+  };
+};
+
 module.exports = {
   up(queryInterface) {
-    return queryInterface.bulkInsert('users', [{ password: '123' }]);
+    const users = [createUser('')];
+    for (let i = 1; i < 5; i++) users.push(createUser(i));
+    return queryInterface.bulkInsert(TABLE_NAME, users);
   },
 
   down(queryInterface) {
-    return queryInterface.bulkDelete('users', null, {});
+    return queryInterface.bulkDelete(TABLE_NAME, null, {});
   },
 };
