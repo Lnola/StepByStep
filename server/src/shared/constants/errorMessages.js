ee5d01de54f573a98339f8af7400609bef429f29
Lnola <nola.luka@gmail.com>
2022-11-09 14:36:48 +0100

    Add some basic error messages

---
+++ server/src/shared/constants/errorMessages.js
@@ -0,0 +1,8 @@
+const errorMessages = {
+  SIGN_IN_ERROR: 'Incorrect user credentials.',
+  REGISTER_ERROR: 'Unable to create user, try different credentials.',
+  FORBIDDEN_ERROR: 'Your access to this resource is forbidden.',
+  TOKEN_EXPIRED_ERROR: 'Token has expired',
+};
+
+export default errorMessages;
