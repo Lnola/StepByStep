d35900311aabb00c8252275237fa3a314d72ccaa
Lnola <nola.luka@gmail.com>
2022-11-09 14:46:22 +0100

    Connect the controller methods with the router

--- server/src/user/index.js
+++ server/src/user/index.js
@@ -1,9 +1,9 @@
-import { fetchById } from './user.controller';
+import { login, logout, register } from './user.controller';
 import { Router } from 'express';
 
 const router = Router();
 const path = '/users';
 
-router.get('/:id', fetchById);
+router.post('/login', login).post('/register', register).post('/logout', logout);
 
 export default { router, path };
