56d6627e2d8af0688d97a9670ebe33ec68bab74c
Lnola <nola.luka@gmail.com>
2023-01-01 21:40:53 +0100

    Add redirect util

--- client/src/utils/routing.js
+++ client/src/utils/routing.js
@@ -3,3 +3,9 @@ import routes from '@/routes';
 export const isRoute = path => {
   return !!routes.find(route => route.path === path);
 };
+
+export const redirect = name => {
+  const route = routes.find(it => it.name === name);
+  if (!route) return redirect('Home');
+  location.replace(route.path);
+};
