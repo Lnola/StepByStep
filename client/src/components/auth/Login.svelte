177c0b7382adc91f917ac0382221093cde886b2a
Lnola <nola.luka@gmail.com>
2023-01-01 23:18:37 +0100

    Create a functional login form

--- client/src/components/auth/Login.svelte
+++ client/src/components/auth/Login.svelte
@@ -1,11 +1,37 @@
 <script>
+  import { authApi } from '@/api';
+  import { redirect } from '@/utils/routing';
   import { createEventDispatcher } from 'svelte';
+  import StepButton from '../common/Button.svelte';
 
   const dispatch = createEventDispatcher();
+
+  let username = '';
+  let password = '';
+
   const swap = () => dispatch('swap');
+
+  const submit = async () => {
+    try {
+      const user = await authApi.login({ username, password });
+      redirect('Home');
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
 </script>
 
 <main>
-  <div>Login</div>
-  <button on:click={swap}>Swap</button>
+  <h1>Login</h1>
+  <form on:submit|preventDefault={submit}>
+    <label for="username">Username:</label>
+    <input bind:value={username} type="text" name="username" placeholder="Username..." />
+    <label for="password">Password:</label>
+    <input bind:value={password} type="password" name="password" placeholder="Password..." />
+    <StepButton type="submit">Login</StepButton>
+    <p>
+      Don't have an account?
+      <StepButton on:click={swap} inline>Click here to register.</StepButton>
+    </p>
+  </form>
 </main>
