3b4f83041e1bef52105e9e41e77c15f667ee614c
Jakov <sikiric.jakov@outlook.com>
2023-01-03 12:24:54 +0100

    fixed query (isPublished included), put in try-catch

--- server/src/recipe/recipe.controller.js
+++ server/src/recipe/recipe.controller.js
@@ -2,9 +2,17 @@ import { OK } from 'http-status';
 import { Recipe } from '@/shared/database/index';
 
 const list = async (req, res, next) => {
-  const recipes = await Recipe.findAll();
-
-  return res.status(OK).json(recipes);
+  try {
+    const recipes = await Recipe.findAll({
+      where: {
+        isPublished: true,
+      },
+    });
+    return res.status(OK).json(recipes);
+  } catch (err) {
+    console.log(err);
+    return next(new Error());
+  }
 };
 
 export { list };
