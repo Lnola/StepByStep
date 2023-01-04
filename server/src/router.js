321fb9a266ed0d144e271509bb80fe8e5b0c4009
Karlito16 <karlo.dimjasevic@gmail.com>
2023-01-04 02:58:42 +0100

    Initial upload.

--- server/src/router.js
+++ server/src/router.js
@@ -1,9 +1,10 @@
+import ingredient from './ingredient/index';
 import { Router } from 'express';
-
 import user from './user/index';
 
 const router = Router();
 
 router.use(user.path, user.router);
+router.use(ingredient.path, ingredient.router);
 
 export default router;
