5f5bcb6fc94fa9e75b2d758b065c0ce0a4d7e5bb
Karlito16 <karlo.dimjasevic@gmail.com>
2023-01-04 15:09:13 +0100

    Initial upload for step controller.

--- server/src/router.js
+++ server/src/router.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-
+import step from './step/index';
 import user from './user/index';
 
 const router = Router();
 
 router.use(user.path, user.router);
+router.use(step.path, step.router);
 
 export default router;
