7136a59231d615e4cd1bee53b6f6353ad4db9125
Karlito16 <karlo.dimjasevic@gmail.com>
2023-01-04 03:05:07 +0100

    Initial upload.

--- server/src/router.js
+++ server/src/router.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-
+import unitOfMeasurement from './unitOfMeasurement/index';
 import user from './user/index';
 
 const router = Router();
 
 router.use(user.path, user.router);
+router.use(unitOfMeasurement.path, unitOfMeasurement.router);
 
 export default router;
