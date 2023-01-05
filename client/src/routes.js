cb0e0a3e2b8015d0b8bc8e831c545867f3153c5b
Lnola <nola.luka@gmail.com>
2023-01-05 22:23:59 +0100

    Sort routes imports

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,10 +1,9 @@
-import { isAuthRoute, isRoute, redirect } from './utils/router/routing';
-
 import Auth from './components/auth/Auth.svelte';
 import Browse from './components/browseRecipes/Browse.svelte';
 import BrowseUserRecipes from './components/browseUserRecipes/BrowseUserRecipes.svelte';
 import Home from './components/home/Home.svelte';
 import { isLoggedIn } from './stores/auth';
+import { isAuthRoute, isRoute, redirect } from './utils/router/routing';
 
 const routes = [
   {
