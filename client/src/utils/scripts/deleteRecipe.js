88a69965948da8937d1306f2ca20e2a6b8b9182e
Lnola <nola.luka@gmail.com>
2023-01-17 22:48:24 +0100

    Fix recipeApi name

--- client/src/utils/scripts/deleteRecipe.js
+++ client/src/utils/scripts/deleteRecipe.js
@@ -1,9 +1,9 @@
-import { recipesApi } from '@/api';
+import { recipeApi } from '@/api';
 import { redirect } from '../router/routing';
 
 export async function deleteRecipe(recipeId) {
   if (recipeId) {
-    await recipesApi.deleteRecipe({ recipeId });
+    await recipeApi.deleteRecipe({ recipeId });
     redirect('UserRecipes');
   }
 }
