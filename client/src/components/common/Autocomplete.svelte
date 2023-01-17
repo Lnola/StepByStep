bf27330df109a41fdc016d39c20c2bc5108f5072
Karlito16 <karlo.dimjasevic@gmail.com>
2023-01-17 19:52:56 +0100

    Initial upload for autocomplete component.

---
+++ client/src/components/common/Autocomplete.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from 'svelte';
+
+  export let source;
+  export let id;
+
+  function setAutocomplete() {
+    window.$(document).ready(
+      window.$(() => {
+        window.$(`#${id}`).autocomplete({
+          source: source
+        })
+      })
+    );
+  }
+
+  onMount(setAutocomplete);
+</script>
+
+<div class="ui-widget">
+  <label for="tags">Tags: </label>
+  <input id={id} />
+</div>
