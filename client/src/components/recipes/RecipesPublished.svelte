28369299070c7aa1780393542b5835df89cc0e35
mar_in <mc53264@fer.hr>
2023-01-19 22:18:51 +0100

    Admin can now delete published recipes and set recipe's attribute published to false.

--- client/src/components/recipes/RecipesPublished.svelte
+++ client/src/components/recipes/RecipesPublished.svelte
@@ -7,7 +7,8 @@
   let category = 0;
   let recipes = [];
   let categories;
-  const shouldDisplayBonusActions = false;
+  let userRole = JSON.parse(localStorage.getItem('user')).role.name;
+  const shouldDisplayBonusActions = userRole === 'admin' ? true : false;
 
   function updateCategory({ detail: { value } }) {
     category = value;
@@ -17,11 +18,24 @@
     recipes = await recipeApi.fetchPublished();
     categories = await categoryApi.fetchAll();
   });
+
+  const remove = async ({ detail: { id } }) => {
+    if (!id) return alert('Delete failed, try again');
+    await recipeApi.remove(id);
+    recipes = await recipeApi.fetchPublished();
+  };
+
+  const updateIsPublished = async ({ detail: { id, isPublished } }) => {
+    await recipeApi.updateIsPublished(id, isPublished);
+    const recipe = recipes.find(recipe => recipe.id === id);
+    recipe.isPublished = !recipe.isPublished;
+    recipes = await recipeApi.fetchPublished();
+  };
 </script>
 
 <main>
   <Filter {categories} on:update={updateCategory} />
-  <Recipes {recipes} {category} {shouldDisplayBonusActions} />
+  <Recipes {recipes} {category} {shouldDisplayBonusActions} on:remove={remove} on:update={updateIsPublished} />
 </main>
 
 <style>
