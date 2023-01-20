ffed1beb1ae680b9aa1abe45f275a859bc6a7ec9
Lnola <nola.luka@gmail.com>
2023-01-20 14:14:56 +0100

    Remove admin checking from admin controller

--- server/src/admin/admin.controller.js
+++ server/src/admin/admin.controller.js
@@ -1,18 +1,13 @@
-import { FORBIDDEN, NOT_FOUND, OK } from 'http-status';
+import { NOT_FOUND, OK } from 'http-status';
 import { DatabaseError } from 'sequelize';
 import errorMessages from '@/shared/constants/errorMessages';
 import HttpError from '@/shared/error/httpError';
 import { User } from '@/shared/database/index';
 
 const allUsers = async (req, res, next) => {
-  const isAdmin = req.user.role.name;
   try {
-    if (isAdmin === 'admin') {
-      const users = await User.findAll();
-      return res.status(OK).json(users);
-    } else {
-      return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN_ERROR));
-    }
+    const users = await User.findAll();
+    return res.status(OK).json(users);
   } catch (error) {
     if (error instanceof DatabaseError) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND_ERROR));
     return next(new Error());
@@ -21,16 +16,11 @@ const allUsers = async (req, res, next) => {
 
 const deleteUser = async (req, res, next) => {
   const { userId } = req.params;
-  const isAdmin = req.user.role.name;
   try {
-    if (isAdmin === 'admin') {
-      const user = await User.findByPk(userId);
-      if (!user) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND_ERROR));
-      await user.destroy();
-      return res.sendStatus(OK);
-    } else {
-      return next(new HttpError(FORBIDDEN, errorMessages.FORBIDDEN_ERROR));
-    }
+    const user = await User.findByPk(userId);
+    if (!user) return next(new HttpError(NOT_FOUND, errorMessages.NOT_FOUND_ERROR));
+    await user.destroy();
+    return res.sendStatus(OK);
   } catch (error) {
     return next(new Error());
   }
